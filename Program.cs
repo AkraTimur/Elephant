2e2a64473d6b14e026ef46ead56ea7ec827b8737
AkraTimur <timur.akra@gmail.com>
2022-07-15 19:14:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Elephant
+{
+    public class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My ears are " + EarSize + " inches tall");
+        }
+        
+
+        static void Main(string[] args)
+        {
+            
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            Elephant swapper = new Elephant();
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+            Console.Write("You pressed ");
+            string line = Console.ReadLine();
+            if (!int.TryParse(line, out int userChoise))
+            {
+                Console.WriteLine("take right number");
+            }
+            
+
+
+                switch (userChoise)
+                {
+                    case 1:
+                        Console.WriteLine("Calling lloyd.WhoAmI");
+                        lloyd.WhoAmI();
+                        break;
+                    case 2:
+                        Console.WriteLine("Calling lucinda.WhoAmI");
+                        lucinda.WhoAmI();
+                        break;
+                    case 3:
+                        Console.WriteLine("References have been swapped");
+                        
+                        swapper = lloyd;
+                        lloyd = lucinda;
+                        lucinda = swapper;
+                        break;
+
+                    default:
+                        Console.WriteLine("Error! Try again");
+                        break;
+                }
+            }
+
+        }
+    }
+}
